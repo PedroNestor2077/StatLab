e27a634aef4ec7dda818ed78f82315e22defb18a
PedroNestor2077 <contato.pedronestor@gmail.com>
2021-02-17 12:53:29 -0300

    home finished

--- slide.js
+++ slide.js
@@ -1,13 +1,13 @@
 window.onresize=()=>{
-	BoxMargin=1440
-	wrapper.style.marginLeft='1440px'
+	BoxMargin=wrapper.clientWidth*4
+	let value=(BoxMargin-wrapper.clientWidth).toString()+'px'
+	wrapper.style.marginLeft=value
 }
 
 function Next(){
 	let boxSize=wrapper.clientWidth
 	const maxMargin=(boxSize*2)
 	if (BoxMargin>0){
-		
 		let value=(BoxMargin-boxSize).toString()+'px'
 		BoxMargin=(BoxMargin-boxSize)
 		wrapper.style.marginLeft=value
@@ -21,13 +21,13 @@ function Prev(){
 		BoxMargin=(BoxMargin+boxSize)
 		wrapper.style.marginLeft=value
 	}
-	console.log(BoxMargin)
 }
 var next=document.getElementById('next')
 var prev=document.getElementById('prev')
 var exprements=document.getElementById('experiments')
 var wrapper=document.getElementById('wrapper')
-var BoxMargin=1440
+var BoxMargin=wrapper.clientWidth*3
+
 
 next.addEventListener('click',Next)
 prev.addEventListener('click',Prev)
